3c592989395859b6b1bbfb31cd2902fc634f32b9
kbihm <kbihm@gmail.com>
2020-05-10 13:00:56 +0530

    changes ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,9 +22,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { Device } from '@ionic-native/device/ngx';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
